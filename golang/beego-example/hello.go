bdbbbd5cc90f4bfd11df02c23e7b487c50928a8e
slpcat <slpcat@qq.com>
2018-09-17 15:09:50 +0800

    beego example

--- golang/beego-example/hello.go
+++ golang/beego-example/hello.go
@@ -1,19 +1,18 @@
 package main
-
-import "github.com/astaxie/beego"
-
-//通过组合的形式在自定义的Controller中将beego.Controller包含，
-//这样自定义的Controller就有了Post,Get...等方法，我们只要重写
-//Get方法，在Get中写入 "hello world"就可以了
+ 
+import (
+            "github.com/astaxie/beego"
+       )
+ 
 type MainController struct {
-    beego.Controller
+        beego.Controller
 }
-
-func (this *MainController)Get(){
-    this.Ctx.WriteString("hello world")
+ 
+func (this *MainController) Get() {
+        this.Ctx.WriteString("hello world")
 }
-
-func main(){
-    beego.Router("/",&MainController{})
-    beego.Run()
+ 
+func main() {
+        beego.Router("/", &MainController{})
+                beego.Run()
 }
