dcb4f352261c0ae96b05084f755d5a49622f6075
若虚 <slpcat@users.noreply.github.com>
2018-09-20 22:10:46 +0800

    Update go_build.sh

--- golang/beego-example/go_build.sh
+++ golang/beego-example/go_build.sh
@@ -3,4 +3,4 @@ source /root/.gvm/scripts/gvm
 gvm use go1.11 --default
 export CGO_ENABLED=0
 go get github.com/astaxie/beego
-go build -o hello hello.go
+go build -ldflags="-s -w" -o hello hello.go
