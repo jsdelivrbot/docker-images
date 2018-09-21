fddaf994503b77066c76cb060bd18fda1f83b15f
slpcat <slpcat@qq.com>
2018-09-21 22:04:23 +0800

    ceph

--- ceph/ubuntu16.04-rbd-provisioner/go_build.sh
+++ ceph/ubuntu16.04-rbd-provisioner/go_build.sh
@@ -6,10 +6,10 @@ export GOOS=linux
 go get github.com/golang/glog
 go get github.com/kubernetes-incubator/external-storage/ceph/rbd/pkg/provision
 go get github.com/kubernetes-incubator/external-storage/lib/controller
-go get github.com/kubernetes-incubator/external-storage/vendor
 go get k8s.io/apimachinery/pkg/util/wait
 go get k8s.io/client-go/kubernetes
 go get k8s.io/client-go/rest
 go get k8s.io/client-go/tools/clientcmd
-go build -a -ldflags '-extldflags "-static"' -o /rbd-provisioner /main.go
+cd /external-storage/ceph/rbd
+go build -a -ldflags '-extldflags "-static"' -o /rbd-provisioner ./cmd/rbd-provisioner
 #go build -ldflags="-s -w" -o hello hello.go
